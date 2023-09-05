6ebc441b04d4bdb72da6fec295288f01c7f59d95
Jiahan Xie <jx353@cornell.edu>
2023-09-05 16:31:24 -0400

    simple DCE iterate until convergence

--- lesson3/global_unused_instrs.cpp
+++ lesson3/global_unused_instrs.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <set>
 
+bool programChanged = false;
+
 bool removeNullValues(Json::Value&);
 
 int main(int argc, char* argv[]) {
@@ -20,37 +22,47 @@ int main(int argc, char* argv[]) {
     Json::Value brilProg;
     jsonFile >> brilProg;
 
-    // First iteration through the whole program to store all used variables
-    std::set<std::string> usedVars;
-    for (int fcnIdx = 0; fcnIdx < brilProg["functions"].size(); ++fcnIdx) {
-        const Json::Value brilInstrucions =
-            brilProg["functions"][fcnIdx]["instrs"];
-        for (int instrIdx = 0; instrIdx < brilInstrucions.size(); ++instrIdx) {
-            if (brilInstrucions[instrIdx].isMember("args")) {
-                for (auto arg : brilInstrucions[instrIdx]["args"]) {
-                    usedVars.insert(arg.asString());
+    do {
+        // First iteration through the whole program to store all used variables
+        std::set<std::string> usedVars;
+        for (int fcnIdx = 0; fcnIdx < brilProg["functions"].size(); ++fcnIdx) {
+            const Json::Value brilInstrucions =
+                brilProg["functions"][fcnIdx]["instrs"];
+            for (int instrIdx = 0; instrIdx < brilInstrucions.size();
+                 ++instrIdx) {
+                if (brilInstrucions[instrIdx].isMember("args")) {
+                    for (auto arg : brilInstrucions[instrIdx]["args"]) {
+                        usedVars.insert(arg.asString());
+                    }
                 }
             }
         }
-    }
 
-    // Second iteration through the whole program to eliminate the instruction
-    // that has dead variable(s) and has no side effects
-    for (int fcnIdx = 0; fcnIdx < brilProg["functions"].size(); ++fcnIdx) {
-        Json::Value& brilInstrucions = brilProg["functions"][fcnIdx]["instrs"];
-        for (int instrIdx = 0; instrIdx < brilInstrucions.size(); ++instrIdx) {
-            if (brilInstrucions[instrIdx].isMember("dest") &&
-                (usedVars.find(brilInstrucions[instrIdx]["dest"].asString()) ==
-                 usedVars.end())) {  // bril has value operations vs. effect
+        // Second iteration through the whole program to eliminate the
+        // instruction that has dead variable(s) and has no side effects
+        bool changedFlag = false;
+        for (int fcnIdx = 0; fcnIdx < brilProg["functions"].size(); ++fcnIdx) {
+            Json::Value& brilInstrucions =
+                brilProg["functions"][fcnIdx]["instrs"];
+            for (int instrIdx = 0; instrIdx < brilInstrucions.size();
+                 ++instrIdx) {
+                if (brilInstrucions[instrIdx].isMember("dest") &&
+                    (usedVars.find(
+                         brilInstrucions[instrIdx]["dest"].asString()) ==
+                     usedVars
+                         .end())) {  // bril has value operations vs. effect
                                      // operations; value operations have "dest"
                                      // fields and never have side effects, and
                                      // vice versa for effect operations
-                brilInstrucions[instrIdx].clear();
+                    brilInstrucions[instrIdx].clear();
+                    changedFlag = true;
+                }
             }
         }
-    }
+        programChanged = changedFlag;
+        removeNullValues(brilProg);
+    } while (programChanged);
 
-    removeNullValues(brilProg);
     Json::FastWriter styledWriter;
     outfile << brilProg;
     outfile.close();
