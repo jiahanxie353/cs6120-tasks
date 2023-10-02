321e1e36822bfd3102fdacb617897e2a2872b2b9
jiahanxie353 <jx353@cornell.edu>
2023-10-02 13:53:49 -0400

    count overhead (in terms of newly added instructions percentage)

--- lesson6/lesson6.cpp
+++ lesson6/lesson6.cpp
@@ -1,6 +1,16 @@
 #include "ssa.h"
 #include <fstream>
 
+// calculate the number of instructions in a given bril program
+int countTotalInstrs(json &brilProg) {
+    int res = 0;
+    for (const auto &brilFcn : brilProg.at("functions")) {
+        if (brilFcn.contains("instrs"))
+            res += brilFcn.at("instrs").size();
+    }
+    return res;
+}
+
 int main(int argc, char *argv[]) {
     json brilProg = readJson(argv[1]);
     string mode = argv[2];
@@ -32,5 +42,11 @@ int main(int argc, char *argv[]) {
         result["functions"] = json::array();
         result["functions"].push_back(fcnJson);
     }
-    *os_ptr << result.dump(4);
+    *os_ptr << result.dump(4) << std::endl;
+
+    int origInstrsCnt = countTotalInstrs(brilProg);
+    int newInstrsCnt = countTotalInstrs(result);
+    std::cout << "The percentage of newly added instructions is: "
+              << (float)(newInstrsCnt - origInstrsCnt) / (float)origInstrsCnt
+              << std::endl;
 }
