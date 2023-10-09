a03390c160dcffc87626bbc7e526d5062646de37
jiahanxie353 <jx353@cornell.edu>
2023-10-09 12:33:25 -0400

    dump type and create shared type ptr

--- type.cpp
+++ type.cpp
@@ -1,1 +1,14 @@
 #include "type.h"
+#include <stdexcept>
+
+using std::string;
+
+std::shared_ptr<Type> Type::createType(const string typeStr) {
+  if (typeStr == "int") {
+    return std::make_shared<IntType>();
+  } else if (typeStr == "bool") {
+    return std::make_shared<BoolType>();
+  } else {
+    throw std::runtime_error("Invalid type: " + typeStr);
+  }
+}
--- type.h
+++ type.h
@@ -1,15 +1,42 @@
 #pragma once
+#include <memory>
+#include <string>
 
-class Type {};
+class Type {
+public:
+  static std::shared_ptr<Type> createType(const std::string typeStr);
+  virtual ~Type() = default;
+  virtual const std::string dump() const = 0;
+};
 
-class PrimType : public Type {};
+class PrimType : public Type {
+public:
+  const std::string dump() const override { return "primitive"; }
+};
 
-class IntType : public PrimType {};
+class IntType : public PrimType {
+public:
+  const std::string dump() const override { return "int"; }
+};
 
-class BoolType : public PrimType {};
+class BoolType : public PrimType {
+public:
+  const std::string dump() const override { return "bool"; }
+};
 
-class VoidType : public Type {};
+class VoidType : public Type {
+public:
+  const std::string dump() const override { return "void"; }
+};
 
-class FcnType : public Type {};
+class LitType : public Type {
+public:
+  const std::string dump() const override { return "literal"; }
+};
+
+class FcnType : public Type {
+public:
+  const std::string dump() const override { return "function"; }
+};
 
 // `isa`, perhaps?
